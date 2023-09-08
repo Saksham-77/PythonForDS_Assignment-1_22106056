21018c1a7df89f9ebbb4bab3d6b588c9f9e9b687
Saksham <118709539+Saksham-77@users.noreply.github.com>
2023-09-08 18:43:42 +0530

    Add files via upload

---
+++ Q5.py
@@ -0,0 +1,14 @@
+str = input("Enter the numbers: ")
+splt = str.split()
+lst = []
+for i in splt:
+    lst.append(int(i))
+for _ in range(len(splt)):
+    m = min(lst[_:])
+    if lst[_] == m:
+        pass
+    else:
+        index = lst.index(m)
+        lst[index] = lst[_]
+        lst[_] = m
+print(lst)
