d56a7d8fb9f3c525933ce72cbd75d36da2387278
Saksham <118709539+Saksham-77@users.noreply.github.com>
2023-09-08 18:34:56 +0530

    Q5.py

--- Q5.py
+++ Q5.py
@@ -1,11 +1,14 @@
 str = input("Enter the numbers: ")
 splt = str.split()
+lst = []
+for i in splt:
+    lst.append(int(i))
 for _ in range(0, len(splt)):
-    if _ == len(splt) - 1:
-        break
+    m = min(lst[_:])
+    if lst[_] == m:
+        pass
     else:
-        m = min(splt[_:])
-        indx = splt.index(m)
-        splt[indx] = splt[_]
-        splt[_] = m
-print(splt)
+        index = lst.index(m)
+        lst[index] = lst[_]
+        lst[_] = m
+print(lst)
